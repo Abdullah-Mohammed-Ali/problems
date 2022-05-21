23e7d66a7ff29640cf764aecc316ca6a030ea6d0
Abdullah Mohammed <32640450+abdostor@users.noreply.github.com>
2022-05-22 00:15:08 +0200

    Time: 3 ms (5.64%), Space: 43.2 MB (75.61%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -2,12 +2,7 @@ class Solution {
     public int search(int[] nums, int target) {
         int first = 0;
         int last = nums.length -1;
-       return binarySearch(nums , first , last , target);
-    }
- public int binarySearch (int[] nums , int first , int last,int target)
-    {
-     
-        while(first <= last){
+         while(first <= last){
             int mid = (first + last) / 2;
             System.out.println(mid);
             if(nums[mid] == target) return mid ;
@@ -15,6 +10,7 @@ class Solution {
             if (nums[mid] > target)   last = mid-1;
         }
         return -1 ;
-
+     
     }
+
 }
